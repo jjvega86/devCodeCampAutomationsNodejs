47e166862beed76d4e0879aaadc744b71b49690b
JJ <jj.vega86@gmail.com>
2021-09-14 19:48:29 -0400

    Added multiple filtering functions to pare down database pages

--- index.js
+++ index.js
@@ -12,18 +12,40 @@ const notion = new Client({ auth: process.env.NOTION_KEY })
 
 const databaseId = process.env.NOTION_DATABASE_ID
 
-// TODO: Write out sequence of functions to get all pages in a database and modify their multi select property with another value
-// TODO: Port this sequence to modifying Active Students to Not Started
 //! awaiting this call to getDatabaseById in order to get resolved data instead of pending Promise
+//TODO: Write a filter database query function that passes an object in for the filter
 const database = await getDatabaseById(notion, databaseId)
 
-const updateAllTags = async (databaseData, notionClient) => {
-  databaseData.forEach(async page => {
+//TODO: Write a function to filter pages for Full-Time or Part-Time Course
+//TODO: Destructure key properties from pages objects returned from database query; refactor functions for simpler logic
+//* Can do this as a database query with a filter object passed as argument
+const filterPagesBySelectProperty = (data, property, value) => {
+  let filteredData = data.filter(page => {
+    if (page.properties[property].select.name === value) {
+      return true
+    }
+  })
+
+  return filteredData
+}
+
+let results = filterPagesBySelectProperty(database, "Course", "Full-Time")
+console.log(results.length)
+let nextResults = filterPagesBySelectProperty(
+  results,
+  "Standup Status",
+  "In Progress"
+)
+console.log(nextResults.length)
+
+//TODO: Write unit tests for all functions
+const updateStandupStatusToNotStarted = async (data, notionClient) => {
+  data.forEach(async page => {
     try {
       let response = await notionClient.pages.update({
         page_id: page.id,
         properties: {
-          Tags: { select: { name: "A new tag" } },
+          "Standup Status": { select: { name: "Not Started" } },
         },
       })
       console.log(response)
@@ -33,4 +55,4 @@ const updateAllTags = async (databaseData, notionClient) => {
   })
 }
 
-updateAllTags(database, notion)
+updateStandupStatusToNotStarted(nextResults, notion)
