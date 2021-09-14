4c928e6ce615a2337490f5a733a568b10fc8e9ab
JJ <jj.vega86@gmail.com>
2021-09-14 15:52:40 -0400

    Added Prettier config, initial API call to add item to test database, verified integration complete

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Converting existing Python based client to a Nodejs SDK with plans to integrate with Pipedream's API connection service for serverless trigger based workflows and scheduled actions
 Eventually splitting into a microservice based architecture with hopes to run on a cloud platform independently
+
+NOTES:
+
+- Using ES6 modules (this is set by adding the "type" property to package.json and setting the value to "module")
--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-require("dotenv").config();
+import dotenv from "dotenv"
+import { Client } from "@notionhq/client"
 
-console.log(process.env.NOTION_KEY);
+dotenv.config()
+
+const notion = new Client({ auth: process.env.NOTION_KEY })
+
+const databaseId = process.env.NOTION_DATABASE_ID
+
+const addItem = async text => {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    })
+    console.log("Success!", response.properties)
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+
+addItem("This is a test")
