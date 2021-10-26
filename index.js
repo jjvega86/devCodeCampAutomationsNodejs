9d32caf93e808f153e022ab924d095264bfbc59b
JJ <jj.vega86@gmail.com>
2021-10-26 08:22:25 -0400

    Added Jest to project for testing, edited Babel configuration to allow for ES6 modules

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv"
 import { Client } from "@notionhq/client"
 
+const notion = new Client({ auth: process.env.NOTION_KEY })
+dotenv.config()
+
 import { getDatabaseById } from "./modules/notion/notionQuery.js"
 import { convertDataToNotionEvents } from "./modules/helpers/courseTemplateParse.js"
 import { addEventsToDatabase } from "./modules/notion/notionCreate.js"
@@ -9,12 +12,8 @@ import { parseActiveStudentsCSV } from "./modules/helpers/activeStudentsParse.js
 import { addStudentsToDatabase } from "./modules/notion/notionAddStudent.js"
 import addDatesToClassSchedule from "./modules/addDatesToClassSchedule.js"
 
-dotenv.config()
-
 //! ALL TODO COMMENTS HERE
-//TODO: Write unit tests for all functions using Jest
-//TODO: Move class schedule functions into own module and expose a single function to use in index.js
+//TODO: Write unit tests for all applicable functions using Jest
+//TODO: Refactor modules folder to organize helpers and notion files together by action
+//* See addDatesToClassSchedule as example. Each one should export a single default function
 //TODO: Write script to add grading templates for all assignments for a Cohort to Grading. (Set up Notion to auto hide until two weeks out in Not Started)
-
-const notion = new Client({ auth: process.env.NOTION_KEY })
-addDatesToClassSchedule(notion)
