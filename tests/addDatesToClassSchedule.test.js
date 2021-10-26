6cf9f4a84d3bb8a6ac1886af970a1bba35cdcfa2
JJ <jj.vega86@gmail.com>
2021-10-26 08:36:19 -0400

    Removed sum test files

--- tests/addDatesToClassSchedule.test.js
+++ tests/addDatesToClassSchedule.test.js
@@ -1,9 +1,14 @@
 import { isWeekend } from "../modules/addDatesToClassSchedule"
 
+// All related tests are grouped under the same describe
+// Each individual test is written under a call to test
+// For readability, use the arrange -> act -> assert structure
 describe("Date helper function tests", () => {
   test("Returns true when date is a Saturday (6)", () => {
-    // arrange and act
+    // arrange
     let date = new Date("2021/10/30")
+
+    // act
     let result = isWeekend(date)
 
     // assert
