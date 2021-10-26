966b21d8c0755688a3d544604cea728843ee837b
JJ <jj.vega86@gmail.com>
2021-10-26 07:55:22 -0400

    Moved all class schedule date logic to own module, exposed a default function that orchestrates all date adding logic

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import dotenv from "dotenv"
 import { Client } from "@notionhq/client"
-import promptSync from "prompt-sync"
-const prompt = promptSync()
 
 import { getDatabaseById } from "./modules/notion/notionQuery.js"
 import { convertDataToNotionEvents } from "./modules/helpers/courseTemplateParse.js"
@@ -9,127 +7,14 @@ import { addEventsToDatabase } from "./modules/notion/notionCreate.js"
 import { updateStandupStatusToNotStarted } from "./modules/notion/notionUpdate.js"
 import { parseActiveStudentsCSV } from "./modules/helpers/activeStudentsParse.js"
 import { addStudentsToDatabase } from "./modules/notion/notionAddStudent.js"
+import addDatesToClassSchedule from "./modules/addDatesToClassSchedule.js"
 
 dotenv.config()
 
 //! ALL TODO COMMENTS HERE
 //TODO: Write unit tests for all functions using Jest
-//TODO: Write script to add dates to class schedule in Class Schedule Template
+//TODO: Move class schedule functions into own module and expose a single function to use in index.js
 //TODO: Write script to add grading templates for all assignments for a Cohort to Grading. (Set up Notion to auto hide until two weeks out in Not Started)
 
-/* 
-1. Query user for class start date and type of course (full or part time) DONE
-2. Grab all class events from Class Schedule Template database in Notion DONE
-3. Parse data to create simpler objects for each event (pageId, type, day, last working day) DONE
-4. Starting on Day 1, add the correct date to each class event. Determine Date object for class start date DONE
-5. When the event day changes, change the active date as well DONE
-6. Check for weekend and holidays, skip those days (and Fridays if part time) DONE
-7. If Assignment event, calculate the last working day and assignment submission date, add to event DONE
-8. Make API requests to update each class event with the dates generated
-*/
-
 const notion = new Client({ auth: process.env.NOTION_KEY })
-const testDatabaseId = process.env.NOTION_DATABASE_ID
-const classScheduleDbId = process.env.NOTION_CLASS_SCHEDULE_ID
-
-const getClassSchedule = async (client, databaseId) => {
-  try {
-    let response = await client.databases.query({
-      database_id: databaseId,
-      sorts: [{ property: "Day", direction: "ascending" }],
-    })
-    return response.results
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const parseClassScheduleEvents = events => {
-  let parsedDatabase = events.map(page => {
-    return {
-      pageId: page.id,
-      day: page.properties.Day.number,
-      type: page.properties.Type.select.name,
-      lastWorkingDay: page.properties["Last Working Day"].number,
-      name: page.properties.Name.title[0].text.content,
-    }
-  })
-  return parsedDatabase
-}
-
-function addDatesToClassEvents(events, classDates) {
-  let eventsWithDates = events.map(event => {
-    //TODO: Refactor using Map with class dates
-    if (event.day !== event.lastWorkingDay) {
-      return {
-        ...event,
-        start: classDates.get(event.day),
-        end: classDates.get(event.lastWorkingDay),
-      }
-    } else {
-      return { ...event, start: classDates.get(event.day) }
-    }
-  })
-  return eventsWithDates
-}
-
-function populateClassDatesMap(date, finalClassDay, formatDate) {
-  let classDates = new Map()
-  for (let index = 1; index <= finalClassDay; index++) {
-    classDates.set(index, formatDate(date))
-    date.setDate(date.getDate() + 1)
-    // add day. Then check if day is a weekend or holiday. If either condition is true, keep incrementing days (always checking for both for each day)
-    while (isWeekend(date) || isHoliday(date)) {
-      date.setDate(date.getDate() + 1)
-    }
-  }
-  return classDates
-}
-
-function isWeekend(date) {
-  if (date.getDay() === 0 || date.getDay() === 6) {
-    return true
-  } else {
-    return false
-  }
-}
-
-function isHoliday(date) {
-  let holidays = ["2021-11-25", "2021-11-26"]
-  if (holidays.includes(formatDate(date))) {
-    return true
-  } else {
-    return false
-  }
-}
-function formatDate(date) {
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${
-    date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
-  }`
-}
-
-const updateClassEvents = async (client, eventsWithDates) => {
-  eventsWithDates.forEach(event => {
-    try {
-      client.pages.update({
-        page_id: event.pageId,
-        properties: {
-          "Date Assigned": {
-            date: {
-              start: event.start,
-              end: event.end ? event.end : null,
-            },
-          },
-        },
-      })
-    } catch (error) {
-      console.error(error)
-    }
-  })
-}
-
-let database = await getClassSchedule(notion, classScheduleDbId)
-let parsedEvents = parseClassScheduleEvents(database)
-let classDates = populateClassDatesMap(new Date("2021/10/18"), 45, formatDate)
-let eventsWithDates = addDatesToClassEvents(parsedEvents, classDates)
-updateClassEvents(notion, eventsWithDates)
+addDatesToClassSchedule(notion)
---
+++ modules/addDatesToClassSchedule.js
@@ -0,0 +1,127 @@
+import PromptSync from "prompt-sync"
+const prompt = PromptSync()
+
+const getClassSchedule = async (client, databaseId) => {
+  // retrieve class schedule from Notion, sorted by day ascending for ease of adding dates
+  try {
+    let response = await client.databases.query({
+      database_id: databaseId,
+      sorts: [{ property: "Day", direction: "ascending" }],
+    })
+    return response.results
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const parseClassScheduleEvents = events => {
+  // whittle down raw Notion data into objects with only the necessary properties
+  let parsedEvents = events.map(page => {
+    return {
+      pageId: page.id,
+      day: page.properties.Day.number,
+      type: page.properties.Type.select.name,
+      lastWorkingDay: page.properties["Last Working Day"].number,
+      name: page.properties.Name.title[0].text.content,
+    }
+  })
+  return parsedEvents
+}
+
+function populateClassDatesMap(date, finalClassDay, formatDate) {
+  // takes all days in a course and adds each as a key to the map. Value is the date of that course day.
+  let classDates = new Map()
+  for (let index = 1; index <= finalClassDay; index++) {
+    classDates.set(index, formatDate(date))
+    date.setDate(date.getDate() + 1)
+    // add day. Then check if day is a weekend or holiday. If either condition is true, keep incrementing days (always checking for both for each day)
+    while (isWeekend(date) || isHoliday(date)) {
+      date.setDate(date.getDate() + 1)
+    }
+  }
+  return classDates
+}
+
+function isWeekend(date) {
+  if (date.getDay() === 0 || date.getDay() === 6) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function isHoliday(date) {
+  // add any holidays in the year using strings in the below array
+  let holidays = ["2021-11-25", "2021-11-26"]
+  if (holidays.includes(formatDate(date))) {
+    return true
+  } else {
+    return false
+  }
+}
+function formatDate(date) {
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${
+    date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
+  }`
+}
+
+function addDatesToClassEvents(events, classDates) {
+  // classDates is Map of key (day number) values (dates of course day as ISO strings)
+  // add start and end dates with each event. Add end only if lastWorkingDay is not the same as Day #
+  let eventsWithDates = events.map(event => {
+    if (event.day !== event.lastWorkingDay) {
+      return {
+        ...event,
+        start: classDates.get(event.day),
+        end: classDates.get(event.lastWorkingDay),
+      }
+    } else {
+      return { ...event, start: classDates.get(event.day) }
+    }
+  })
+  return eventsWithDates
+}
+
+const updateClassEvents = async (client, eventsWithDates) => {
+  // takes in events with start/end dates added as strings and makes PATCH requests to Notion API to update events with date properties
+  eventsWithDates.forEach(event => {
+    try {
+      client.pages.update({
+        page_id: event.pageId,
+        properties: {
+          "Date Assigned": {
+            date: {
+              start: event.start,
+              end: event.end ? event.end : null,
+            },
+          },
+        },
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  })
+}
+
+const addDatesToClassSchedule = async client => {
+  let userInput = prompt("What date does the class start? <YYYY/MM/DD>")
+  let numberOfClassDays = parseInt(prompt("How many days are in the course?"))
+
+  let notionEvents = await getClassSchedule(
+    client,
+    process.env.NOTION_CLASS_SCHEDULE_ID
+  )
+
+  let parsedEvents = parseClassScheduleEvents(notionEvents)
+  let classDates = populateClassDatesMap(
+    new Date(userInput),
+    numberOfClassDays,
+    formatDate
+  )
+
+  let eventsWithDates = addDatesToClassEvents(parsedEvents, classDates)
+  updateClassEvents(client, eventsWithDates)
+  process.on("exit", () => console.log("Dates added to class schedule!"))
+}
+
+export { addDatesToClassSchedule as default }
